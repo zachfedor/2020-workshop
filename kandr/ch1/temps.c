c08271b8457876abce0ef11c35f9b245d7433c08
Zach Fedor <zachfedor@gmail.com>
2020-03-21 11:46:59 -0400

    K&R 1.2 Exercises: Floats vs Ints

--- kandr/ch1/temps.c
+++ kandr/ch1/temps.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 
 /* print Fahrenheit-Celsius table
- * for fahr = 0, 20, ..., 300 */
+ * for fahr = 0, 20, ..., 300
+ * with floating point division */
 int main()
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;    /* lower limit of temerature table */
     upper = 300;  /* upper limit */
     step  = 20;   /* step size */
 
+    printf("Fahr   Cels\n");
+    printf("----   ----\n");
+
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%4.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
+    }
+
+    printf("\n\nCels  Fahr\n");
+    printf("----  ----\n");
+
+    lower = -100;
+    upper = 200;
+    celsius = lower;
+
+    while (celsius <= upper) {
+        fahr = (celsius*9)/5 + 32;
+        printf("%4.0f %5.0f\n", celsius, fahr);
+        celsius = celsius + step;
     }
 }
