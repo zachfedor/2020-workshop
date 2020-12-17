975c7af3362e590e9f1441db1c4692439615f861
Zach Fedor <zachfedor@gmail.com>
2020-12-17 11:51:41 -0500

    EloquentJS 20: grep part 2
    Removed resolution of all promises for faster output and to allow
    recursion. Now checking for directories with error catching for a
    non-existent path. Recursive matching requires keeping track of nested
    paths.

--- eloquent-js/20_node/grep.js
+++ eloquent-js/20_node/grep.js
@@ -1,21 +1,32 @@
 /* Grep
  * Given regex pattern and one or more files, find pattern in files
  */
-const { readFile } = require("fs").promises;
+const { readdir, readFile, stat } = require("fs").promises;
+const { join } = require("path");
 
 const pattern = new RegExp(process.argv[2]);
-const targets = process.argv.slice(3);
+const isMatch = async (target) => {
+  return pattern.test(await readFile(target, "utf-8"));
+};
 
-Promise.all(
-  targets.map(async (target) => {
-    // TODO: check for directory
-    const text = await readFile(target, "utf-8");
-    return pattern.test(text) ? target : null;
-  })
-).then((results) => {
-  results
-    .filter((result) => result)
-    .forEach((result) => {
-      console.log(result);
-    });
-});
+const grep = async (targets, cwd = "") => {
+  targets.forEach(async (target) => {
+    let path = join(cwd, target);
+    let stats;
+    try {
+      stats = await stat(path);
+    } catch (error) {
+      if (error.code != "ENOENT") throw error;
+      console.error(`Error: file ${path} doesn't exist`);
+      return;
+    }
+
+    if (stats.isDirectory()) {
+      return grep(await readdir(path), path);
+    }
+
+    if (await isMatch(path)) console.log(path);
+  });
+};
+
+grep(process.argv.slice(3));
