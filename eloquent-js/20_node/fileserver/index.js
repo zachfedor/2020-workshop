abe92757805a40ebba45a5a90716ce979c105efc
Zach Fedor <zachfedor@gmail.com>
2020-12-17 19:40:17 -0500

    EloquentJS 20: fileserver mkcol
    Add a MKCOL (make collection) method to the simple node file server
    created in the book. It accepts a request to a URL and we recursively
    make the directoy(ies) provided, returning a 201 on success. It errors
    if the target already exists as a file.

---
+++ eloquent-js/20_node/fileserver/index.js
@@ -0,0 +1,101 @@
+const { createReadStream, createWriteStream } = require("fs");
+const { mkdir, readdir, rmdir, stat, unlink } = require("fs").promises;
+const { createServer } = require("http");
+const mime = require("mime");
+const { resolve, sep } = require("path");
+const { parse } = require("url");
+
+const baseDirectory = process.cwd();
+const methods = Object.create(null);
+
+createServer((request, response) => {
+  let handler = methods[request.method] || notAllowed;
+  handler(request)
+    .catch((error) => {
+      if (error.status != null) return error;
+      return { body: String(error), status: 500 };
+    })
+    .then(({ body, status = 200, type = "text/plain" }) => {
+      response.writeHead(status, { "Content-Type": type });
+      if (body && body.pipe) body.pipe(response);
+      else response.end(body);
+    });
+}).listen(8000);
+
+async function notAllowed(request) {
+  return {
+    status: 405,
+    body: `Method ${request.method} not allowed.`,
+  };
+}
+
+const urlPath = function (url) {
+  let { pathname } = parse(url);
+  let path = resolve(decodeURIComponent(pathname).slice(1));
+  if (path != baseDirectory && !path.startsWith(baseDirectory + sep)) {
+    throw { status: 403, body: "Forbidden" };
+  }
+  return path;
+};
+
+methods.GET = async function (request) {
+  let path = urlPath(request.url);
+  let stats;
+  try {
+    stats = await stat(path);
+  } catch (error) {
+    if (error.code != "ENOENT") throw error;
+    else return { status: 404, body: "File not found" };
+  }
+  if (stats.isDirectory()) {
+    return { body: (await readdir(path)).join("\n") };
+  } else {
+    return { body: createReadStream(path), type: mime.getType(path) };
+  }
+};
+
+methods.DELETE = async function (request) {
+  let path = urlPath(request.url);
+  let stats;
+  try {
+    stats = await stat(path);
+  } catch (error) {
+    if (error.code != "ENOENT") throw error;
+    else return { status: 204 };
+  }
+  if (stats.isDirectory()) await rmdir(path);
+  else await unlink(path);
+  return { status: 204 };
+};
+
+methods.MKCOL = async function (request) {
+  let path = urlPath(request.url);
+  let stats;
+  try {
+    stats = await stat(path);
+  } catch (error) {
+    if (error.code != "ENOENT") throw error;
+  }
+
+  if (stats && !stats.isDirectory) {
+    return { status: 400, body: "File already exists at path" };
+  }
+
+  await mkdir(path, { recursive: true });
+  return { status: 201 };
+};
+
+const pipeStream = function (from, to) {
+  return new Promise((resolve, reject) => {
+    from.on("error", reject);
+    to.on("error", reject);
+    to.on("finish", resolve);
+    from.pipe(to);
+  });
+};
+
+methods.PUT = async function (request) {
+  let path = urlPath(request.url);
+  await pipeStream(request, createWriteStream(path));
+  return { status: 204 };
+};
